6e56efd980f751b5c05be4c904e5bf6500ba3195
verahector <verahector26@gmail.com>
2018-03-25 19:38:35 -0600

    Initial commit

---
+++ app/src/main/java/com/sanroman/vera/hectorubaldo/searchrecipes/view/SearchView.java
@@ -0,0 +1,15 @@
+package com.sanroman.vera.hectorubaldo.searchrecipes.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sanroman.vera.hectorubaldo.searchrecipes.R;
+
+public class SearchView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_view);
+    }
+}
